647b3a03d340c1c6e3025b0436ea3008832815e8
董小英 <1917594328@qq.com>
2023-02-23 09:31:11 +0800

    第二次提交

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
     <!-- <路由的出口> -->
     <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-shipin"> </i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="box" style="padding: 20px">hello world</div>
   </div>
 </template>
 <script>
@@ -10,4 +19,10 @@ export default {
   name: "App",
 };
 </script>
-<style lang="less"></style>
+<style lang="less">
+.box {
+  width: 200px;
+  height: 100px;
+  background-color: aqua;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// 加载Vant核心组件库
+import Vant from "vant";
+// 加载Vant全局样式
+import "vant/lib/index.css";
+// 注册使用Vant组件库
+Vue.use(Vant);
 // 加载全局样式
 import "./styles/index.less";
+// 加载动态设置REM基准值
+import "amfe-flexible";
+import "postcss-pxtorem";
+
 Vue.config.productionTip = false;
 
 new Vue({
